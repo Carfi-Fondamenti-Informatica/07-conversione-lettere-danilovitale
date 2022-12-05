648a69e24625710f6c7cf8b89fbf744118d87483
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-12-05 10:39:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+
+char b=0;
+
 
 int main(){
-  return 0;
+   
+char a=0;
+   cin>>a;
+   
+   carattere(a);
+   if (carattere(a))
+   {
+      cout << b;
+     
+   }
+   else{
+       cout << "errore";
+   }
+   return 0;
 }
