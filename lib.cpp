b4ee6766a6675d0df055c72ae12a639c43f9fcb4
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-12-05 10:39:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
-
+#include "lib.h"
+bool carattere (char &a)
+{
+   if (a==0||(a>90&&a<97)||(a<65)||a>123){
+       return false;
+      
+   }
+   else{
+       if (a>=65&&a<=90)
+       {
+           b=a+32;
+           
+       }
+        else{
+           b=a-32; 
+           
+        }
+       
+       
+       return true;
+   }
+}
